c99ccc385cb9384834b20f2fb737c0781a2d6917
UgUsDead <hugomorais412010@gmail.com>
2024-12-04 10:41:01 +0000

    "menu"

--- codigo/main.py
+++ codigo/main.py
@@ -1,2 +1,4 @@
-print("Bem vindo à CP!! \n\n 1-Adicionar estação \n\n 2-Adicionar carril \n\n 3-Adicionar Linha \n\n 4- Adicionar Comboio\n\n 5-Adicionar Viagem \n\n 6-Reservar viagem")
+print("Bem vindo à CP!! \n\n 1-Adicionar estação \n\n 2-Adicionar carril \n\n 3-Adicionar Linha \n\n 4- Adicionar Comboio\n\n 5-Adicionar Viagem \n\n 6-Reservar viagem \n\n")
+
+resp=input("Por favor escolha uma opção: ")
 
