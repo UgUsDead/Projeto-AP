10a1124a07be723e8ea1f9d9f86a6d50aab20654
Minimus <minimuspt2@gmail.com>
2024-12-04 12:27:33 +0000

    funçao da estação

--- codigo/my_functions.py
+++ codigo/my_functions.py
@@ -1,11 +1,20 @@
-def estacao(a,b,c,d):
-    return ("swag")
+import os.path
+
+def estacao(cod_est,nome,lati,longi):
+    path = os.path.realpath(__file__)
+    dir=os.path.dirname(path)
+    dir= dir.replace("codigo","dados")
+    os.chdir(dir)
+    tabela_estacoes = open ("tabela_estacoes.txt", "a")
+    cod_est=str(cod_est)
+    nome=str(nome)
+    lati=str(lati)
+    longi=str(longi)
+    texto= cod_est + ";" + nome + ";" + lati + ";" + longi + "\n"
+    myEstacoes = tabela_estacoes.writelines( texto )
+    tabela_estacoes.close()
 
 
-def dor(bv):
-    print("a vida é bela")
+    
 
 
-
-def carril(f,g,z,x):
-    return ("arroz")
